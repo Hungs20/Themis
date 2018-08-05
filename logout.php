2e1f742bef8fa0085a88136b0b22642ceeed7f24
Chu Văn Hưng <tuoitre2k@gmail.com>
2018-08-05 14:29:37 +0700

    Add files via upload
    fix load logout

--- logout.php
+++ logout.php
@@ -1,11 +1,11 @@
+<?php
+	session_start();
+	session_destroy();
+	header("Location: login.php");
+?>
 
 <!-- loading -->
 
 <script src="js/pace.min.js"></script>
 <link rel="stylesheet" href="/css/pace-theme-minimal.css">
 <!--end load-->
-<?php
-	session_start();
-	session_destroy();
-	header("Location: login.php");
-?>
